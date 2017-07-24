2e0975da5f7537bfca3012a1eda79bfe19a59e3d
JamesORourke <geekjamesorourkejr@icloud.com>
2017-07-24 15:16:48 -0700

    Initial checkin

---
+++ MyBirthday.js
@@ -0,0 +1,20 @@
+var daysUntilMyBirthday = 0;
+
+if (daysUntilMyBirthday > 30 )
+{
+  console.log("My birthday is so far away, so I am sad.");
+}
+else if (daysUntilMyBirthday > 5)
+{
+  console.log("I am getting SOOO excited for my birthday!!! ");
+}
+else if (daysUntilMyBirthday > 1 )
+{
+  console.log("SCREAM: I AM SOOO EXCITED FOR MY BIRTHDAY SOON!!! AAHHHHHHAHHHA");
+}
+else
+{
+  console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*");
+  console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪");
+  console.log("*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«");
+}
